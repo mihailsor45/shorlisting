10fd8b076a72d1dd3d4341e7fb2090bd542bad65
Михаил Шорников <mihailsornikov@MacBook-Air-Mihail.local>
2024-05-03 20:22:46 +0300

    Initial commit

---
+++ listingj/Java_listing/src/main/java/Listing2/Listing2_2.java
@@ -0,0 +1,20 @@
+package Listing2;
+
+
+public class Listing2_2 {
+
+    public static void main(String[] args)  {
+        
+        int var1;
+        int var2;
+
+        
+        var1 = 4096;
+        var2 = var1 / 4;
+
+        
+        System.out.println("var1 = " + var2);
+        System.out.println("var2 = var1 / 4 = " + var2);
+
+    } 
+}
